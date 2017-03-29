6c306905949e33b644af3572576ef91cc5762bcb
Ajay NS <ajay.ns@outlook.com>
2017-03-29 14:24:03 +0530

    Update music-player

--- components/ng-musicplayer/assets/app.js
+++ components/ng-musicplayer/assets/app.js
@@ -4,21 +4,27 @@ app.controller('appCtrl', function($scope) {
   $scope.songs = [
     {
       id: 'one',
-      title: 'one',
-      artist: 'one',
+      title: 'The First Track',
+      artist: 'First Artist',
+      album: 'One',
       url: 'http://www.schillmania.com/projects/soundmanager2/demo/_mp3/rain.mp3'
     },
     {
       id: 'two',
-      title: 'two',
-      artist: 'two',
+      title: 'The Second Track',
+      artist: 'Second Artist',
+      album: 'Two',
       url: 'http://www.schillmania.com/projects/soundmanager2/demo/_mp3/walking.mp3'
     },
     {
       id: 'three',
-      title: 'three',
-      artist: 'three',
+      title: 'The Third Track',
+      artist: 'Third Artist',
+      album: 'Three',
       url: 'http://www.freshly-ground.com/misc/music/carl-3-barlp.mp3'
     }
   ]
+
+  $scope.isPlaying = false;
+  $scope.songHover = false;
 })
