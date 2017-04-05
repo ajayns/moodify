05d26c3b988b58c4673c2f24148228239f31d119
Ajay NS <ajay.ns@outlook.com>
2017-04-05 09:12:54 +0530

    Program cam-app

--- components/cam-app/assets/app.js
+++ components/cam-app/assets/app.js
@@ -5,9 +5,23 @@ app.controller('appCtrl', function($scope) {
 
   $scope.snap = function() {
     $scope.cam = false;
+    Webcam.snap(function(data_uri) {
+      console.log('snapped');
+      $scope.imgURI = data_uri;
+    });
   }
 
   $scope.retake = function() {
     $scope.cam = true;
   }
-})
+});
+
+Webcam.set({
+      width: 640,
+      height: 480,
+      image_format: 'jpeg',
+      jpeg_quality: 90
+});
+
+Webcam.attach('#cam');
+
