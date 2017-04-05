40e65411cfbc1d95541ff18b3f0e243300bc9641
Ajay NS <ajay.ns@outlook.com>
2017-04-05 07:06:50 +0530

    Skin cam-app

--- components/cam-app/assets/app.js
+++ components/cam-app/assets/app.js
@@ -1,1 +1,13 @@
+var app = angular.module('app', []);
 
+app.controller('appCtrl', function($scope) {
+  $scope.cam = true;
+
+  $scope.snap = function() {
+    $scope.cam = false;
+  }
+
+  $scope.retake = function() {
+    $scope.cam = true;
+  }
+})
