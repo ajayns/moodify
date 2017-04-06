32a55fd122f9f4a862a3e349ca2b5cf146ccc13b
Ajay NS <ajay.ns@outlook.com>
2017-04-06 09:19:39 +0530

    Init cam-app integration

---
+++ static/js/cam-app.js
@@ -0,0 +1,26 @@
+var app = angular.module('app', []);
+
+app.controller('appCtrl', function($scope) {
+  $scope.cam = true;
+
+  $scope.snap = function() {
+    $scope.cam = false;
+    Webcam.snap(function(data_uri) {
+      $scope.imgURI = data_uri;
+    });
+  }
+
+  $scope.retake = function() {
+    $scope.cam = true;
+  }
+});
+
+Webcam.set({
+      width: 640,
+      height: 480,
+      image_format: 'jpeg',
+      jpeg_quality: 90
+});
+
+Webcam.attach('#cam');
+
