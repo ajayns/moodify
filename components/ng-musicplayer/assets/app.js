a2465977d1e1c950eefd836f317615ba409472ca
Ajay NS <ajay.ns@outlook.com>
2017-04-13 00:35:46 +0530

    Update ng-musicplayer

--- components/ng-musicplayer/assets/app.js
+++ components/ng-musicplayer/assets/app.js
@@ -1,30 +1,36 @@
 var app = angular.module('app', ['angularSoundManager']);
 
-app.controller('appCtrl', function($scope) {
-  $scope.songs = [
-    {
-      id: 'one',
-      title: 'The First Track',
-      artist: 'First Artist',
-      album: 'One',
-      url: 'http://www.schillmania.com/projects/soundmanager2/demo/_mp3/rain.mp3'
-    },
-    {
-      id: 'two',
-      title: 'The Second Track',
-      artist: 'Second Artist',
-      album: 'Two',
-      url: 'http://www.schillmania.com/projects/soundmanager2/demo/_mp3/walking.mp3'
-    },
-    {
-      id: 'three',
-      title: 'The Third Track',
-      artist: 'Third Artist',
-      album: 'Three',
-      url: 'http://www.freshly-ground.com/misc/music/carl-3-barlp.mp3'
-    }
-  ]
+app.controller('appCtrl', function ($scope) {
 
+  // Sample Data
+  $scope.songs = [
+    { id: "1",
+      mood: "happy",
+      title: "Take My Breath Away",
+      artist: "Alesso",
+      url: "src/mp3/1.mp3",
+      albumart: "src/img/1.jpg",
+      album: "Singles"
+    },
+    { id: "2",
+      mood: "happy",
+      title: "Sleepwalker",
+      artist: "Illenium",
+      url: "src/mp3/2.mp3",
+      albumart: "src/img/2.jpg",
+      album: "Ashes"
+    },
+    { id: "3",
+      mood: "happy",
+      title: "Sweet Child O' Mine",
+      artist: "Guns N' Roses",
+      url: "src/mp3/3.mp3",
+      albumart: "src/img/3.jpeg",
+      album: "Singles"
+    }
+  ];
+
+  // Init variables
   $scope.isPlaying = false;
   $scope.songHover = false;
 })
